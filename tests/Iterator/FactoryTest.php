5118eb99c4fc7ef82ec8d904ca239af7f1e97d5b
edorian <github@wallbash.com>
2011-02-22 21:19:17 +0100

    Provided additonal tests for File_Iterator_Factory::getFilesAsArray that make use of the suffix feature

--- tests/Iterator/FactoryTest.php
+++ tests/Iterator/FactoryTest.php
@@ -1,8 +1,8 @@
 <?php
 
-class FactoryTest extends PHPUnit_Framework_TestCase {
+class File_Iterator_FactoryTest extends PHPUnit_Framework_TestCase {
 
-    public function testGetFilesAsArrayWithoutSpecialParametersWorks() {
+    public function testGetFilesAsArrayWithoutAdditionalParametersWorks() {
         $path = dirname(__FILE__)."/_files/twoEmptyPhpFiles"; 
         $files = File_Iterator_Factory::getFilesAsArray($path);
         $expected = array(
@@ -14,5 +14,39 @@ class FactoryTest extends PHPUnit_Framework_TestCase {
         );
         $this->assertEquals($expected, $files);
     }
+
+    public function testGetFilesAsArrayWithoutAdditionParamatersWorksForHiddenFiles() {
+        $path = dirname(__FILE__)."/_files/oneEmtpyPhpFileAndOneSwapFile";
+        $files = File_Iterator_Factory::getFilesAsArray($path);
+        $expected = array(
+            $path."/foo.php",
+            $path."/.foo.php.swp",
+            $path,
+            dirname(__FILE__)."/_files",
+
+        );
+        $this->assertEquals($expected, $files);
+    }
+
+    public function testGetFilesAsArrayWithPHPSuffixWorks() {
+        $path = dirname(__FILE__)."/_files/twoEmptyPhpFiles"; 
+        $files = File_Iterator_Factory::getFilesAsArray($path, array('php'));
+        $expected = array(
+            $path."/foo.php",
+            $path."/bar.php",
+        );
+        $this->assertEquals($expected, $files);
+    }
+
+    public function testGetFilesAsArrayWithPHPSuffixWorksForFoldersWithHiddenFiles() {
+        $path = dirname(__FILE__)."/_files/oneEmtpyPhpFileAndOneSwapFile"; 
+        $files = File_Iterator_Factory::getFilesAsArray($path, array('php'));
+        $expected = array(
+            $path."/foo.php",
+        );
+        $this->assertEquals($expected, $files);
+    }
+
+
 
 }
