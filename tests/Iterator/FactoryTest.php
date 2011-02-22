ae580095168d775a5d295743f3dee9efa2d94661
edorian <github@wallbash.com>
2011-02-22 20:51:06 +0100

    First test to estlish a working baseline, I'm not to sure why it does return 4 entries but the test is just documenting the current state

--- tests/Iterator/FactoryTest.php
+++ tests/Iterator/FactoryTest.php
@@ -2,4 +2,17 @@
 
 class FactoryTest extends PHPUnit_Framework_TestCase {
 
+    public function testGetFilesAsArrayWithoutSpecialParametersWorks() {
+        $path = dirname(__FILE__)."/_files/twoEmptyPhpFiles"; 
+        $files = File_Iterator_Factory::getFilesAsArray($path);
+        $expected = array(
+            $path."/foo.php",
+            $path."/bar.php",
+            $path,
+            dirname(__FILE__)."/_files",
+
+        );
+        $this->assertEquals($expected, $files);
+    }
+
 }
