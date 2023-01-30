5cea92de68129bb63fe6761c9e831552ec906f25
Sebastian Bergmann <sb@sebastian-bergmann.de>
2023-01-30 08:30:25 +0100

    Return early

--- src/Iterator.php
+++ src/Iterator.php
@@ -85,17 +85,13 @@ final class Iterator extends FilterIterator
             return true;
         }
 
-        $matched = false;
-
         foreach ($subStrings as $string) {
             if (($type === self::PREFIX && str_starts_with($filename, $string)) ||
                 ($type === self::SUFFIX && str_ends_with($filename, $string))) {
-                $matched = true;
-
-                break;
+                return true;
             }
         }
 
-        return $matched;
+        return false;
     }
 }
